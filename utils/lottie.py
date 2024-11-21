1f7ae7e1fc92bfa52ff3fd2765a93ed483cc4f4b
Gabriel Koku Kuma <wolakuma@gmail.com>
2024-11-21 11:40:42 +0100

    Update lottie.py

--- utils/lottie.py
+++ utils/lottie.py
@@ -4,14 +4,28 @@ import requests
 from streamlit_lottie import st_lottie
 
 # Create a function to load lottie data from with persistence across multiple pages at session-state
+# @st.cache_data(show_spinner=False, persist=True)
+# def load_lottie_data(url):
+#     response = requests.get(url)
+#     if response.status_code == 200:
+#         return response.json()
+#     else:
+#         st.error(f'Invalid response from {url}')
+#         return None
+
 @st.cache_data(show_spinner=False, persist=True)
 def load_lottie_data(url):
     response = requests.get(url)
     if response.status_code == 200:
-        return response.json()
+        try:
+            return response.json()
+        except json.JSONDecodeError:
+            st.error("Error decoding JSON.")
+            return None
     else:
-        st.error(f'Invalid response from {url}')
+        st.error(f"Invalid response from {url}")
         return None
+
     
 # Display loaded lottie data on various pages
 def display_lottie(page_name):
